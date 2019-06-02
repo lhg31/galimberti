0eac3f948dc5ff619205d30cac6b863b9c2d4f8e
lhg31 <31hik31@gmail.com>
2019-06-02 17:37:12 -0300

    authentication

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+const SECRET = process.env.SECRET || "fuckyou";
+const authCookie = "galimbaToken";
+const uuidv4 = require('uuid/v4');
 
-app.get('/', (req, res) => res.send(process.env));
+function checkAuthentication(req, res, next) {
+    let token = req.cookies[ authCookie ];
+    if (!token) {
+        return res.render( 'login' );
+    }
+    
+    jwt.verify(token, SECRET, function(err, decoded) {
+      if (err) {
+        res.clearCookie( authCookie );
+        return res.render( 'login' );
+      }
+      
+      next();
+    });
+  }
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use( cookieParser() );
+app.use( bodyParser() );
+app.set('view engine', 'ejs');
+
+app.get('/', checkAuthentication, (req, res, next) => res.render('index'));
+
+app.post('/login', (req, res, next) => {
+    //todo, se já logado, redirecionar index
+    if (req.body.pwd === '123') {
+        //auth ok
+        const uuid = uuidv4();
+        var token = jwt.sign({ uuid }, SECRET, {
+            expiresIn: 300 // expires in 5min
+        });
+        res.cookie( authCookie, token );
+        res.status(200).send('Logou!');
+    }
+
+    else{
+        res.status(500).send('Login inválido!');
+    }
+});
+
+app.post('/logout', function (req, res) {
+    res.clearCookie( authCookie );
+    res.status(200).end();
+});
+
+app.listen(port, () => console.log(`Rodando na porta ${port}!`));
